71bc2c110ea342c9c4f06b2ecb61e6a646138360
Anya <anna.artamono@gmail.com>
2022-02-06 06:15:21 +0300

    Тренировки в полёте

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import url_for, Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/<title>')
+@app.route('/index/<title>')
+def index(title):
+    return render_template('base.html', title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
